17446e5c4f57c34d7160fdff6fc4fc651c318db9
Keitaroh Kobayashi <keita@kkob.us>
2012-10-10 22:04:06 +0900

    Added PHP < 5.2.1 compatibility
    Signed-off-by: Keitaroh Kobayashi <keita@kkob.us>

--- wpro.php
+++ wpro.php
@@ -11,6 +11,21 @@ License: GPLv2
 
 // define('WPRO_DEBUG', true);
 
+// PHP < 5.2.1 compatibility
+if ( !function_exists('sys_get_temp_dir')) {
+	function sys_get_temp_dir() {
+		if( $temp = getenv('TMP') ) return $temp;
+		if( $temp = getenv('TEMP') ) return $temp;
+		if( $temp = getenv('TMPDIR') ) return $temp;
+		$temp = tempnam(__FILE__, '');
+		if (file_exists($temp)) {
+			unlink($temp);
+			return dirname($temp);
+		}
+		return null;
+	}
+}
+
 new WordpressReadOnly;
 
 /* * * * * * * * * * * * * * * * * * * * * * *
