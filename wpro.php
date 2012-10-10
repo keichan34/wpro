125db6081403b75ef1e6e68b0ae1f14b1be197a9
Keitaroh Kobayashi <keita@kkob.us>
2012-10-10 22:11:04 +0900

    curl works under open_basedir & safe_mode environments
    Signed-off-by: Keitaroh Kobayashi <keita@kkob.us>

--- wpro.php
+++ wpro.php
@@ -26,6 +26,52 @@ if ( !function_exists('sys_get_temp_dir')) {
 	}
 }
 
+// open_basedir / safe_mode disallows CURLOPT_FOLLOWLOCATION
+function curl_exec_follow($ch, &$maxredirect = null) { 
+    $mr = $maxredirect === null ? 5 : intval($maxredirect); 
+    if (ini_get('open_basedir') == '' && ini_get('safe_mode' == 'Off')) { 
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, $mr > 0); 
+        curl_setopt($ch, CURLOPT_MAXREDIRS, $mr); 
+    } else { 
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); 
+        if ($mr > 0) { 
+            $newurl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL); 
+
+            $rch = curl_copy_handle($ch); 
+            curl_setopt($rch, CURLOPT_HEADER, true); 
+            curl_setopt($rch, CURLOPT_NOBODY, true); 
+            curl_setopt($rch, CURLOPT_FORBID_REUSE, false); 
+            curl_setopt($rch, CURLOPT_RETURNTRANSFER, true); 
+            do { 
+                curl_setopt($rch, CURLOPT_URL, $newurl); 
+                $header = curl_exec($rch); 
+                if (curl_errno($rch)) { 
+                    $code = 0; 
+                } else { 
+                    $code = curl_getinfo($rch, CURLINFO_HTTP_CODE); 
+                    if ($code == 301 || $code == 302) { 
+                        preg_match('/Location:(.*?)\n/', $header, $matches); 
+                        $newurl = trim(array_pop($matches)); 
+                    } else { 
+                        $code = 0; 
+                    } 
+                } 
+            } while ($code && --$mr); 
+            curl_close($rch); 
+            if (!$mr) { 
+                if ($maxredirect === null) { 
+                    trigger_error('Too many redirects. When following redirects, libcurl hit the maximum amount.', E_USER_WARNING); 
+                } else { 
+                    $maxredirect = 0; 
+                } 
+                return false; 
+            } 
+            curl_setopt($ch, CURLOPT_URL, $newurl); 
+        } 
+    } 
+    return curl_exec($ch); 
+}
+
 new WordpressReadOnly;
 
 /* * * * * * * * * * * * * * * * * * * * * * *
@@ -83,8 +129,7 @@ class WordpressReadOnlyBackend extends WordpressReadOnlyGeneric {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_NOBODY, 1);
 		curl_setopt($ch, CURLOPT_URL, $path);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		$result = trim(curl_exec($ch));
+		$result = trim(curl_exec_follow($ch));
 
 		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		$this->debug('-> http return code: ' . $httpCode);
@@ -503,11 +548,10 @@ class WordpressReadOnly extends WordpressReadOnlyGeneric {
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $filepath);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 			curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 
 			$fh = fopen($tmpfile, 'w');
-			fwrite($fh, curl_exec($ch));
+			fwrite($fh, curl_exec_follow($ch));
 			fclose($fh);
 
 			$this->removeTemporaryLocalData($tmpfile);
